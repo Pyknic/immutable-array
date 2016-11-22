a921b95acfe514db8214eb8c2294f3b425070a47
pyknic <emil@speedment.com>
2016-11-22 13:37:57 -0800

    Fix date in license headers

--- src/main/java/com/github/pyknic/immutablearray/BooleanImmutableArray.java
+++ src/main/java/com/github/pyknic/immutablearray/BooleanImmutableArray.java
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright (c) 2006-2016, Emil Forslund. All Rights Reserved.
+ * Copyright (c) 2016-2016, Emil Forslund. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); You may not
  * use this file except in compliance with the License. You may obtain a copy of
